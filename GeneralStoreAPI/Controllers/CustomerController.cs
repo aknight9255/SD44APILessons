35fbabba829f04f3f179caaf24cdfd76da8af8b2
aknight9255 <amanda.knight55@gmail.com>
2020-04-15 12:00:21 -0400

    customer post added

--- GeneralStoreAPI/Controllers/CustomerController.cs
+++ GeneralStoreAPI/Controllers/CustomerController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace GeneralStoreAPI.Controllers
@@ -11,5 +12,16 @@ namespace GeneralStoreAPI.Controllers
     public class CustomerController : ApiController
     {
         private readonly ApplicationDbContext _context = new ApplicationDbContext(); 
+        [HttpPost]
+        public async Task<IHttpActionResult> Post(Customer customer)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Customers.Add(customer);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
     }
 }
